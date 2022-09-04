4fd778ab264fdf6d13d88ecef13b38432705f2f3
pedroth <pedrotiago92@gmail.com>
2022-09-04 10:31:24 +0100

    minor update

--- src/Vec3.js
+++ src/Vec3.js
@@ -173,8 +173,10 @@ export default class Vec {
     return new Vec(_sanitize_input(values, BUILD_VEC(values.length)));
   }
 
-  static ZERO = (n) => (n === 3 ? new Vector3() : new Vec(BUILD_VEC(n)));
+  static ZERO = (n) =>
+    n === 3 ? new Vector3() : n === 2 ? new Vector2() : new Vec(BUILD_VEC(n));
   static e = (n) => (i) => {
+    if (n === 2) return Vector2.e(i);
     if (n === 3) return Vector3.e(i);
     const vec = BUILD_VEC(n);
     if (i >= 0 && i < n) {
@@ -184,6 +186,7 @@ export default class Vec {
   };
 
   static RANDOM = (n) => {
+    if (n === 2) return Vector2.RANDOM();
     if (n === 3) return Vector3.RANDOM();
     const v = BUILD_VEC(n);
     for (let i = 0; i < n; i++) {
@@ -310,15 +313,15 @@ class Vector3 {
     return this.sub(u).length() < precision;
   }
 
+  take(n = 0, m = this._vec.length) {
+    return Vec.of(this.x, this.y, this.z).take(n, m);
+  }
+
   findIndex(predicate) {
     if (predicate(this.x)) return 0;
     if (predicate(this.y)) return 1;
     if (predicate(this.z)) return 2;
     return -1;
-  }
-
-  take(n = 0, m = 3) {
-    return Vec.of(this.x, this.y, this.z).take(n, m);
   }
 
   static fromArray(array) {
@@ -444,14 +447,14 @@ class Vector2 {
     return this.sub(u).length() < precision;
   }
 
+  take(n = 0, m = this._vec.length) {
+    return Vec.of(this.x, this.y).take(n, m);
+  }
+
   findIndex(predicate) {
     if (predicate(this.x)) return 0;
     if (predicate(this.y)) return 1;
     return -1;
-  }
-
-  take(n = 0, m = 2) {
-    return Vec.of(this.x, this.y).take(n, m);
   }
 
   static fromArray(array) {
